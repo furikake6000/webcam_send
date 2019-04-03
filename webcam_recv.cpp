8b4b57cf76544ad49a0d29c60ad7088d94eca4cc
furikake6000 <ewave63@keio.jp>
2019-04-03 17:19:07 +0900

    don't use string to cache bytes

--- webcam_recv.cpp
+++ webcam_recv.cpp
@@ -18,8 +18,7 @@ using namespace std;
 
 int main(){
     char imgbuf[WIDTH * HEIGHT * 3];
-    std::string imgbufstr;
-    int imgbufsize;
+    int imgbuf_head;
     cv::Mat received_frame;
 
     char tmpbuf[1024]; // temporary buffer
@@ -48,32 +47,15 @@ int main(){
     }
 
     while(1){
-        /*imgbufsize = 0;
-        while(imgbufsize < WIDTH * HEIGHT * 3 - 1){
-            if(imgbufsize + sizeof(tmpbuf) > WIDTH * HEIGHT * 3 - 1){
-                // Finishing frame load
-                int remainsize = WIDTH * HEIGHT * 3 - 1 - imgbufsize;
-                memset(tmpbuf, 0, sizeof(tmpbuf));
-                recvfrom(s, tmpbuf, remainsize, 0, (struct sockaddr *)&from_addr, &sin_size);
-            }else{
-                recvfrom(s, tmpbuf, sizeof(tmpbuf), 0, (struct sockaddr *)&from_addr, &sin_size);
-            }
-            
-            
-        }*/
-        imgbufstr = "";
-        while(imgbufstr.length() < WIDTH * HEIGHT * 3){
-            int remainsize = min(WIDTH * HEIGHT * 3 - (int)imgbufstr.length(), 1024);
+
+        imgbuf_head = 0;
+        while(imgbuf_head < WIDTH * HEIGHT * 3){
+            int remainsize = min(WIDTH * HEIGHT * 3 - imgbuf_head, 1024);
 
             recvfrom(s, tmpbuf, remainsize, 0, (struct sockaddr *)&from_addr, &sin_size);
-            string tmpdatastr= tmpbuf;
-            tmpdatastr = tmpdatastr.substr(0, remainsize);
-            imgbufstr += tmpdatastr;
+            strncpy(&imgbuf[imgbuf_head], tmpbuf, remainsize);
         }
 
-        printf("Received %d bytes.", (int)imgbufstr.length());
-
-        strncpy(imgbuf, imgbufstr.c_str(), WIDTH * HEIGHT * 3);
         received_frame = cv::Mat(HEIGHT, WIDTH, CV_8UC3, imgbuf);
 
         cv::imshow("camera capture", received_frame);
