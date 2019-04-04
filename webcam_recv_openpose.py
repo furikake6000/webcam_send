6977a77a23a3207c7ccdeeb36a8bfd2d88fac7ba
furikake6000 <ewave5000@gmail.com>
2019-04-05 01:19:21 +0900

    Fixed openpose loaderror

--- webcam_recv_openpose.py
+++ webcam_recv_openpose.py
@@ -9,6 +9,7 @@ import numpy as np
 import cv2
 import threading
 import struct
+import argparse
 
 from socket import socket, AF_INET, SOCK_STREAM
 
@@ -29,7 +30,7 @@ try:
         sys.path.append('/usr/local/python');
         # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
         # sys.path.append('/usr/local/python')
-        from openpose import pyopenpose as op
+        from openpose import *
 except ImportError as e:
     print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
     raise e
